7c3804c8068b25b5bd7231188d76c9b981b206d6
stefanom13 <stefanomancuso13@gmail.com>
2022-03-09 08:42:02 +0100

    creazione repo

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="">
+    header
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyHeader',
+    
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
